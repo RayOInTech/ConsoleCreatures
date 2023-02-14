f59575369e06217c74c9c7cd300a14c8b22cd944
Ray Omoregie <118464830+RayOInTech@users.noreply.github.com>
2023-02-13 22:00:54 -0800

    Add files via upload
    my program written in C#

---
+++ ConsoleCreatures.cs
@@ -0,0 +1,38 @@
+using System;
+    //Here is my first Console Creature names Spooky the Ghost.
+      Console.WriteLine("  .-.");
+      Console.WriteLine(" (o o)");
+      Console.WriteLine(" | O |");
+      Console.WriteLine(" |   | ");
+      Console.WriteLine(" '~~~' ");
+    
+    Console.WriteLine("My name is Spooky the Ghost\n");
+
+    //Here is my second Console Creature the Angry Volcano.
+    Console.WriteLine("   (   )");
+    Console.WriteLine("  ( (  )  )");
+    Console.WriteLine(" (   (    )  )");
+    Console.WriteLine("     ( (   )    )");
+    Console.WriteLine("       (   )");
+    Console.WriteLine("/---------------\\");
+    Console.WriteLine("/      \\  //   \\");
+    Console.WriteLine("/       0 0     \\");
+    Console.WriteLine("/        o       \\");
+    Console.WriteLine("*******************");
+
+
+    Console.WriteLine("My name is Lava and I'm an angry volcanco.");
+ 
+ namespace C___Code_Files
+{
+
+    class myConsoleClass
+  {
+    static void Main()
+    {
+
+            }
+    }
+    }
+
+
